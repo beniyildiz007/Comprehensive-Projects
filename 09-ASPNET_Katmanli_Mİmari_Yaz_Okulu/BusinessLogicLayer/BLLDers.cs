000d088a932edaed15c15cd1313221fd6fd056f3
Berkan Nihat YILDIZ <95151751+beniyildiz007@users.noreply.github.com>
2023-04-18 00:38:55 +0300

    Add files via upload

---
+++ 09-ASPNET_Katmanli_Mİmari_Yaz_Okulu/BusinessLogicLayer/BLLDers.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EntityLayer;
+using DataAccessLayer;
+
+namespace BusinessLogicLayer
+{
+    public class BLLDers
+    {
+        public static List<EntityDers> BllListele()
+        {
+            return DALDers.DersListesi();
+        }
+
+        public static int BLLTalepEkle(EntityBasvuruForm p)
+        {
+            if(p.BASOGRID!=null && p.BASDERSID != null)
+            {
+                return DALDers.TalepEkle(p);
+            }
+            return -1;
+        }
+    }
+}
---
+++ 09-ASPNET_Katmanli_Mİmari_Yaz_Okulu/BusinessLogicLayer/BLLOgrenci.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EntityLayer;
+using DataAccessLayer;
+
+namespace BusinessLogicLayer
+{
+    public class BLLOgrenci
+    {
+        public static int OgrenciEkleBLL(EntityOgrenci p)
+        {
+            if(p.AD!=null && p.SOYAD!=null && p.NUMARA!=null && p.SIFRE!=null && p.FOTOGRAF != null)
+            {
+                return DALOgrenci.OgrenciEkle(p);
+            }
+            return -1;
+        }
+
+        public static List<EntityOgrenci> BllListele()
+        {
+            return DALOgrenci.OgrenciListesi();
+        }
+
+        public static bool BLLOgrenciSil(int p)
+        {
+            if (p != null)
+            {
+                return DALOgrenci.OgrenciSil(p);
+            }
+            return false;
+        }
+
+        public static List<EntityOgrenci> BLLDetay(int p)
+        {
+            return DALOgrenci.OgrenciDetay(p);
+        }
+
+        public static bool BLLOgrenciGuncelle(EntityOgrenci p)
+        {
+            if (p.AD != null && p.SOYAD != null && p.NUMARA != null && p.SIFRE != null && p.FOTOGRAF != null && p.OGRID > 0)
+            {
+                return DALOgrenci.OgrenciGuncelle(p);
+            }
+            return false;
+        }
+
+    }
+}
